03f87aa6e9e69871438d63ac5c5ce74999fd654f
nx-xn2002 <1870230468@qq.com>
2024-03-17 14:42:48 +0800

    README

--- src/test/java/com/nx/nxbi/MainApplicationTests.java
+++ src/test/java/com/nx/nxbi/MainApplicationTests.java
@@ -1,25 +1,21 @@
 package com.nx.nxbi;
 
 import com.nx.nxbi.config.WxOpenConfig;
+
 import javax.annotation.Resource;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 /**
  * 主类测试
- *
- * 
- * 
  */
 @SpringBootTest
 class MainApplicationTests {
 
-    @Resource
-    private WxOpenConfig wxOpenConfig;
-
     @Test
     void contextLoads() {
-        System.out.println(wxOpenConfig);
+
     }
 
 }
