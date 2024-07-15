bcdd66a9a2f4eab9fb0a39371cee65077408bf8a
nx-xn2002 <1870230468@qq.com>
2024-07-15 18:58:57 +0800

    调整errorcode，添加限流相关报错

--- src/main/java/com/nx/nxbi/common/ErrorCode.java
+++ src/main/java/com/nx/nxbi/common/ErrorCode.java
@@ -1,19 +1,25 @@
 package com.nx.nxbi.common;
 
+import lombok.Getter;
+
 /**
  * 自定义错误码
  *
- * 
- * 
+ * @author Ni Xiang
  */
+@Getter
 public enum ErrorCode {
 
+    /**
+     * 错误码
+     */
     SUCCESS(0, "ok"),
     PARAMS_ERROR(40000, "请求参数错误"),
     NOT_LOGIN_ERROR(40100, "未登录"),
     NO_AUTH_ERROR(40101, "无权限"),
     NOT_FOUND_ERROR(40400, "请求数据不存在"),
     FORBIDDEN_ERROR(40300, "禁止访问"),
+    TOO_MANY_REQUEST(42900, "请求过于频繁"),
     SYSTEM_ERROR(50000, "系统内部异常"),
     OPERATION_ERROR(50001, "操作失败");
 
@@ -30,14 +36,6 @@ public enum ErrorCode {
     ErrorCode(int code, String message) {
         this.code = code;
         this.message = message;
-    }
-
-    public int getCode() {
-        return code;
-    }
-
-    public String getMessage() {
-        return message;
     }
 
 }
